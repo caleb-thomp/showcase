c8b98b146dd044ef31955b4c4bf6cc396f962267
Caleb Thompson <caleb.softdev@gmail.com>
2023-04-21 22:51:11 +0000

    added nuxt1

---
+++ nuxt1/components/Tutorial.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+ 
+      <div class=" bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Caleb's Nuxt Apps
+        </h2>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalebNuxt'
+}
+</script>
