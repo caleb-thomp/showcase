1a713ea1a7b40d4eae5f84c2bc305543b50da7c4
Caleb Thompson <caleb.softdev@gmail.com>
2023-04-25 22:18:04 +0000

    rust init

---
+++ README.md
@@ -0,0 +1,1 @@
+# ShowCase Rust Branch
---
+++ prog1/main.rs
@@ -0,0 +1,8 @@
+// Print a Random Number
+use rand::Rng;
+
+fn main() {
+    let mut range = rand::thread_rng();
+    let num: i32 = range.gen();
+    println!("Random: {}", num);
+}
